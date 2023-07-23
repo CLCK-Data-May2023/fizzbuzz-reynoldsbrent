39d0e761f00003d4e0243f61a66662702281f8ac
Brent Reynolds <brentsreynolds5@gmail.com>
2023-07-22 20:18:15 -0400

    Fizzbuzz logic

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
 # add your code here
+count = 1
 
+while count <= 100:
+    if count % 3 == 0 and count % 5 == 0:
+        print("FizzBuzz")
+    elif count % 3 == 0:
+        print("Fizz")
+    elif count % 5 == 0:
+        print("Buzz")
+    else:
+        print(count)
+    count += 1
